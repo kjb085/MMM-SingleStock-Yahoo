1ab826434b7f9f7ec4f9c122c8e374f4fbb6e6fc
Kenn Breece <kbreece.dev@gmail.com>
2025-04-13 15:31:28 -0400

    Switch to Yahoo API

--- MMM-SingleStock.js
+++ MMM-SingleStock.js
@@ -1,28 +1,26 @@
 /* global Module, Log */
 
-/* Magic Mirror Module: MMM-SingleStock (https://github.com/balassy/MMM-SingleStock)
- * By György Balássy (https://www.linkedin.com/in/balassy)
+/* Magic Mirror Module: MMM-SingleStock-Yahoo (https://github.com/kjb085/MMM-SingleStock-Yahoo)
  * MIT Licensed.
  */
 
-Module.register('MMM-SingleStock', {
+Module.register("MMM-SingleStock-Yahoo", {
   defaults: {
-    stockSymbol: 'GOOG',
-    apiToken: '',
+    stockSymbol: "GOOG",
     updateInterval: 3600000,
     showChange: true,
-    changeType: '',
+    changeType: "",
     colorized: false,
     minimal: false,
-    label: 'symbol' // 'symbol' | 'companyName' | 'none'
+    label: "symbol", // 'symbol' | 'companyName' | 'none'
   },
 
-  requiresVersion: '2.1.0',
+  requiresVersion: "2.1.0",
 
   getTranslations() {
     return {
-      en: 'translations/en.json',
-      hu: 'translations/hu.json'
+      en: "translations/en.json",
+      hu: "translations/hu.json",
     };
   },
 
@@ -39,53 +37,56 @@ Module.register('MMM-SingleStock', {
   },
 
   getDom() {
-    const wrapper = document.createElement('div');
+    const wrapper = document.createElement("div");
 
     if (this.viewModel) {
-      const priceEl = document.createElement('div');
+      const priceEl = document.createElement("div");
       if (this.config.minimal) {
-        priceEl.classList = 'small';
+        priceEl.classList = "small";
       }
 
-      const labelEl = document.createElement('span');
+      const labelEl = document.createElement("span");
       labelEl.innerHTML = `${this.viewModel.label}`;
       priceEl.appendChild(labelEl);
 
-      const valueEl = document.createElement('span');
+      const valueEl = document.createElement("span");
       valueEl.innerHTML = ` ${this.viewModel.price}`;
       if (this.config.colorized) {
-        valueEl.classList = 'bright';
+        valueEl.classList = "bright";
       }
       priceEl.appendChild(valueEl);
 
       wrapper.appendChild(priceEl);
 
       if (this.config.showChange) {
-        const changeEl = document.createElement('div');
+        const changeEl = document.createElement("div");
 
-        changeEl.innerHTML = this.config.changeType === 'percent'
-          ? `(${this.viewModel.change}%)`
-          : `(${this.viewModel.change})`;
+        changeEl.innerHTML =
+          this.config.changeType === "percent"
+            ? `(${this.viewModel.change}%)`
+            : `(${this.viewModel.change})`;
 
         changeEl.classList = this.config.minimal
-          ? 'dimmed xsmall'
-          : 'dimmed small';
+          ? "dimmed xsmall"
+          : "dimmed small";
 
         if (this.config.colorized) {
           if (this.viewModel.change > 0) {
-            changeEl.style = 'color: #a3ea80';
+            changeEl.style = "color: #a3ea80";
           }
           if (this.viewModel.change < 0) {
-            changeEl.style = 'color: #FF8E99';
+            changeEl.style = "color: #FF8E99";
           }
         }
 
         wrapper.appendChild(changeEl);
       }
     } else {
-      const loadingEl = document.createElement('span');
-      loadingEl.innerHTML = this.translate('LOADING', { symbol: this.config.stockSymbol });
-      loadingEl.classList = 'dimmed small';
+      const loadingEl = document.createElement("span");
+      loadingEl.innerHTML = this.translate("LOADING", {
+        symbol: this.config.stockSymbol,
+      });
+      loadingEl.classList = "dimmed small";
       wrapper.appendChild(loadingEl);
     }
 
@@ -95,17 +96,20 @@ Module.register('MMM-SingleStock', {
   _getData(onCompleteCallback) {
     const self = this;
 
-    const url = `https://cloud.iexapis.com/v1/stock/${this.config.stockSymbol}/quote?token=${this.config.apiToken}`;
+    const url = `https://query2.finance.yahoo.com/v8/finance/chart/${this.config.stockSymbol}`;
 
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
+    xhr.open("GET", url, true);
     xhr.onreadystatechange = function onReadyStateChange() {
       if (this.readyState === 4) {
         if (this.status === 200) {
           self._processResponse(this.response);
           onCompleteCallback();
         } else {
-          Log.error(self.name, `MMM-SingleStock: Failed to load data. XHR status: ${this.status}`);
+          Log.error(
+            self.name,
+            `MMM-SingleStock-Yahoo: Failed to load data. XHR status: ${this.status}`
+          );
         }
       }
     };
@@ -115,29 +119,33 @@ Module.register('MMM-SingleStock', {
 
   _processResponse(responseBody) {
     const response = JSON.parse(responseBody);
+    const result = response.chart.results[0].meta;
+    const price = result.regularMarketPrice;
+    const prevClose = result.chartPreviousClose;
+    const change = price - prevClose;
 
     this.viewModel = {
-      price: response.latestPrice
+      price: price,
     };
 
     switch (this.config.changeType) {
-      case 'percent':
-        this.viewModel.change = (response.changePercent * 100).toFixed(2);
+      case "percent":
+        this.viewModel.change = ((change / price) * 100).toFixed(2);
         break;
       default:
-        this.viewModel.change = response.change > 0 ? `+${response.change}` : `${response.change}`;
+        this.viewModel.change = change > 0 ? `+${change}` : `${change}`;
         break;
     }
 
     switch (this.config.label) {
-      case 'symbol':
-        this.viewModel.label = response.symbol;
+      case "symbol":
+        this.viewModel.label = result.symbol;
         break;
-      case 'companyName':
-        this.viewModel.label = response.companyName;
+      case "companyName":
+        this.viewModel.label = result.shortName;
         break;
-      case 'none':
-        this.viewModel.label = '';
+      case "none":
+        this.viewModel.label = "";
         break;
       default:
         this.viewModel.label = this.config.label;
@@ -149,5 +157,5 @@ Module.register('MMM-SingleStock', {
     }
 
     this.hasData = true;
-  }
+  },
 });
